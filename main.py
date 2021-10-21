c73ccba29ec54bf52d3d1faeeaf9097d7b8ba8ec
EvgeniiRomanov <e78rus@mail.ru>
2021-10-21 16:15:24 +0300

    Проблемы с диаг и вертик у X, e  норм

--- main.py
+++ main.py
@@ -22,53 +22,63 @@ def print_start_pole():
     print(" "*30, "-"*3, pole_[6]," "*1, pole_[7]," "*1, pole_[8],"-"*3)
     print(" "*30, "-" * 17, "\n")
 
-# Функция сравнения занятости введенного поля
-# def print_game_pole(zanytost):
-#
-#     pole_zan_ = list(range(1, 10))
-#     if pole_zan_[zanytost] == zanytost:
-#
-#
-#     for value in range(len(pole_zan_)):
-#         if zanytost > pole_zan_[value]
-#     else:
-#         print("Введенная ячейка" +str(zanytost) +"уже занята, будьте внимательнее выберите другую ячейку")
-    # pole_ = list(range(1,10))
-    # print(" "*30, "-" * 17)
-    # print(" "*30, "-"*3, pole_[0]," "*1, pole_[1]," "*1, pole_[2],"-"*3)
-    # print(" "*30, "-"*3, pole_[3]," "*1, pole_[4]," "*1, pole_[5],"-"*3)
-    # print(" "*30, "-"*3, pole_[6]," "*1, pole_[7]," "*1, pole_[8],"-"*3)
-    # print(" "*30, "-" * 17, "\n")
+def print_game_pole():
 
+#    pole_ = list(range(1,10))
+    print(" "*30, "-" * 17)
+    print(" "*30, "-"*3, pole_global[0]," "*1, pole_global[1]," "*1, pole_global[2],"-"*3)
+    print(" "*30, "-"*3, pole_global[3]," "*1, pole_global[4]," "*1, pole_global[5],"-"*3)
+    print(" "*30, "-"*3, pole_global[6]," "*1, pole_global[7]," "*1, pole_global[8],"-"*3)
+    print(" "*30, "-" * 17, "\n")
 
-# # Функция проверки занятости поля
-# # Функция проверки выигрыша
-# #
-#
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print('Hi', name)  # Press Ctrl+F8 to toggle the breakpoint.
-#
-#
-# # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
+# Функция проверяет комбинации выигрыша
+def who_winner(a):
 
-#print_instruction()
-#print_start_pole()
+    if a == 'X':
+        if (pole_global[0] == pole_global[1] == pole_global[2] == 'X') or (         #Проверка горизонталей
+            pole_global[3] == pole_global[4] == pole_global[5] == 'X') or (
+            pole_global[6] == pole_global[7] == pole_global[8] == 'X') or (
+            pole_global[0] == pole_global[3] == pole_global[6] == 'X') or (         #Проверка вертикалей
+            pole_global[1] == pole_global[4] == pole_global[7] == 'X') or (
+            pole_global[2] == pole_global[5] == pole_global[8] == 'X') or (
+            pole_global[0] == pole_global[4] == pole_global[8] == 'X') or (         #Проверка диагоналей
+            pole_global[2] == pole_global[4] == pole_global[6] == 'X'):
+
+            return a
+
+    elif a == 'A':
+        if (pole_global[0] == pole_global[1] == pole_global[2] == 'A') or (         #Проверка горизонталей
+            pole_global[3] == pole_global[4] == pole_global[5] == 'A') or (
+            pole_global[6] == pole_global[7] == pole_global[8] == 'A') or (
+            pole_global[0] == pole_global[3] == pole_global[6] == 'A') or (         #Проверка вертикалей
+            pole_global[1] == pole_global[4] == pole_global[7] == 'A') or (
+            pole_global[2] == pole_global[5] == pole_global[8] == 'A') or (
+            pole_global[0] == pole_global[4] == pole_global[8] == 'A') or (         #Проверка диагоналей
+            pole_global[2] == pole_global[4] == pole_global[6] == 'A'):
+
+            return a
 
 pole_global = list(range(1,10))  #Глобальное поле
-
 print(pole_global)
+print_start_pole()
 
 for index in range(len(pole_global)):
     if index % 2 == 0:  #ход игрока №1 (нечетные ходы)
+        # !!! Тут надо как-то завернуть все это дело в функцию
         first_gamer = int(input("Игрок №1 введите номер поля, где будет поставлен Х: "))
-        if 1 <= first_gamer <= 9:                   # Проверяем что число соответствует номеру ячейки
-            if pole_global[first_gamer-1] == first_gamer:
-                pole_global[first_gamer-1] = 'X'
-                print(pole_global)
-                #print("Установили", pole_global[first_gamer-1], "в ячейку", first_gamer-1, "\n", pole_global)
+        if 1 <= first_gamer <= 9:                           # Проверяем что число соответствует номеру ячейки
+            if pole_global[first_gamer-1] == first_gamer:   # Если ячейка не занята
+                pole_global[first_gamer-1] = 'X'            # Записываем значение в ячейку
+                c = who_winner('X')                                            # Проверяем условие выигрыша
+                if c == 'X':
+                    print("Вы победили")
+                    print(pole_global)
+                    print_game_pole()
+                    break
+                else:
+                    print("Продолжаем игру")
+                    print(pole_global)
+                    print_game_pole()
             else:
                 print("Поле уже занято, введите другой номер поля")
         else:
@@ -77,11 +87,22 @@ for index in range(len(pole_global)):
         second_gamer = int(input("Игрок №2 введите номер поля, где будет поставлен O: "))
         if 1 <= second_gamer <= 9:  # Проверяем что число соответствует номеру ячейки
             if pole_global[second_gamer - 1] == second_gamer:
-                pole_global[second_gamer - 1] = 'O'
-                print(pole_global)
-                # print("Установили", pole_global[second_gamer-1], "в ячейку", second_gamer-1, "\n", pole_global)
+                pole_global[second_gamer - 1] = 'A'
+                c1 = who_winner('A')  # Проверяем условие выигрыша
+                if c1 == 'A':
+                    print("Вы победили")
+                    print(pole_global)
+                    print_game_pole()
+                    break
+                else:
+                    print("Продолжаем игру")
+                    print(pole_global)
+                    print_game_pole()
             else:
                 print("Поле уже занято, введите другой номер поля")
         else:
             print("Вы ввели неверный номер поля, будьте внимательнее и повторите ход!")
 
+#       Игра
+
+#print_instruction()
